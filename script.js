b84d92181e0a1f6aab5399f546b8e2e96affa1f6
autumn034 <l324018852@outlook.com>
2023-06-06 19:35:47 -0400

    Add UI

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 // grid creation
-let grid = document.querySelector('.grid');
-for (let i=0; i<16; i++) {
-    let row = document.createElement('div');
-    row.cssText = "line-height: 0px;";
-    for (let j=0; j<16; j++) {
-        let box = document.createElement('div');
-        box.classList.add('box');
-        row.appendChild(box);
+function createGrid(range) {
+    let boxHeightWidth =  512 / range;
+    let grid = document.querySelector('.grid');
+    grid.innerHTML = ''; // clear grid
+    for (let i=0; i<range; i++) {
+        let row = document.createElement('div');
+        row.cssText = "line-height: 0px;";
+        for (let j=0; j<range; j++) {
+            let box = document.createElement('div');
+            box.classList.add('grid__box');
+            box.style.width = boxHeightWidth + "px";
+            box.style.height = boxHeightWidth + "px";
+            
+            box.addEventListener('mouseover', () => {
+                box.style.background = 'black';
+            });
+
+            row.appendChild(box);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
 }
 
-// change box color upon mouse over
-let boxes = document.querySelectorAll('.box');
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', (e) => {
-        e.target.style.background = 'black';
+let changeSizeBtn = document.querySelector('#changeSize');
+changeSizeBtn.addEventListener('click', () => {
+    let newRange = prompt("Enter new grid size; 16 = 16x16 grid: ", "16");
+
+    if (newRange > 100) {
+        newRange = 100;
+    } else if (!newRange > 0) { // ensure int and not negative
+        newRange = 16;
+    }
+
+    createGrid(newRange);
+});
+
+let clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    let boxes = document.querySelectorAll('.grid__box');
+    boxes.forEach((box) => {
+        box.style.backgroundColor = 'white';
     });
 });
+
+// inital grid
+createGrid(16);
