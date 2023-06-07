db145859d5b7bd2813943eb59f47ea4920499699
autumn034 <l324018852@outlook.com>
2023-06-06 21:15:07 -0400

    Add rainbow paint color, media-query. Improve UI.

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
+let rainbowMode = false;
+let defaultPaintColor = '#161616';
+
 // grid creation
 function createGrid(range) {
-    let boxHeightWidth =  512 / range;
     let grid = document.querySelector('.grid');
+    grid.style.gridTemplateColumns = `repeat(${range}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${range}, 1fr)`;
+    
     grid.innerHTML = ''; // clear grid
-    for (let i=0; i<range; i++) {
-        let row = document.createElement('div');
-        row.cssText = "line-height: 0px;";
-        for (let j=0; j<range; j++) {
-            let box = document.createElement('div');
-            box.classList.add('grid__box');
-            box.style.width = boxHeightWidth + "px";
-            box.style.height = boxHeightWidth + "px";
-            
-            box.addEventListener('mouseover', () => {
-                box.style.background = 'black';
-            });
+    for (let i=0; i < range * range; i++) {
+        let box = document.createElement('div');
+        box.classList.add('grid__box');
 
-            row.appendChild(box);
-        }
-        grid.appendChild(row);
+        box.addEventListener('mouseover', () => {
+            box.style.background = defaultPaintColor;
+        });
+
+        grid.appendChild(box);        
     }
 }
 
@@ -42,6 +40,30 @@ clearBtn.addEventListener('click', () => {
         box.style.backgroundColor = 'white';
     });
 });
+
+let rainbowBtn = document.querySelector('#rainbow');
+rainbowBtn.addEventListener('click', () => {
+    let boxes = document.querySelectorAll('.grid__box');    
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', () => {    
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);    
+            box.style.backgroundColor = `rgb(${r},${g},${b})`;
+        });
+    });
+});
+
+let blackBtn = document.querySelector('#black');
+blackBtn.addEventListener('click', () => {
+    let boxes = document.querySelectorAll('.grid__box');    
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', () => {     
+            box.style.backgroundColor = defaultPaintColor;
+        });
+    });
+});
+
 
 // inital grid
 createGrid(16);
